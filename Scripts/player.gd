ad896ca9b4f95c8d0d54eda7c680177b5bfa46bc
Okerlund <okerlund84@gmail.com>
2025-11-03 14:55:57 -0600

    Added left right movement for the player.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,20 @@
 extends CharacterBody2D
 
-@export var move_speed : float = 500
+@export var move_speed : float = 300
+@export var acceleration : float = 50
+@export var braking : float = 20
 @export var jump_height : float = 100
 
 var move_input
 var gravity_flipped : bool = false
 
 func _physics_process(delta: float) -> void:
-	pass
+
+	move_input = Input.get_axis("move_left", "move_right")
+
+	if move_input != 0:
+		velocity.x = lerp(velocity.x, move_input * move_speed, acceleration * delta)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, braking * delta)
+
+	move_and_slide()
